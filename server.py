738bd5720c268c8c672ac78cec2559461972aa30
v-chigur <v-chigur@mail.ru>
2017-01-08 17:04:55 +0300

    Add cookies

--- server.py
+++ server.py
@@ -7,21 +7,31 @@ class MainHandler(tornado.web.RequestHandler):
 		id = self.get_argument('id', '')
 
 		if id != '':
-			name = id.split('.')[0]
-			f = open('votes.json')
-			text = f.read()
-			f.close()
-
-			votes_list = json.loads(text)
-			if name in votes_list.keys():
-				votes_list[name] += 1
+			info = self.get_cookie('cookies') #чтобы один пользователь не мог голосовать дважды
+			if info != None:
+				cookies = info.split(',')
 			else:
-				votes_list[name] = 1
+				cookies = []
 
-			text = json.dumps(votes_list)
-			f = open("votes.json", 'w')
-			f.write(text)
-			f.close()
+			if not (id in cookies):
+				f = open('votes.json')
+				text = f.read()
+				f.close()
+	
+				votes_list = json.loads(text)
+				if id in votes_list.keys():
+					votes_list[id] += 1
+				else:
+					votes_list[id] = 1
+	
+				text = json.dumps(votes_list)
+				f = open('votes.json', 'w')
+				f.write(text)
+				f.close()
+				cookies.append(str(id))
+				c = ','.join(cookies)
+				self.set_cookie('cookies', c)
+
 
 		name1 = str(random.randint(1, 10))
 		name2 = str(random.randint(1, 10))
@@ -29,7 +39,7 @@ class MainHandler(tornado.web.RequestHandler):
 		while name1 == name2:
 			name2 = str(random.randint(1, 10))
 
-		self.render('facemash.html', name1 = name1 + '.jpg', name2 = name2 + '.jpg')
+		self.render('facemash.html', name1 = name1, name2 = name2)
 
 settings = [
 	('/', MainHandler),
